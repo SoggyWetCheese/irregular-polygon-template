5b242e0fb05690e373906f27684b076d7f659d71
SoggyWetCheese <geniusboy93195m@gmail.com>
2025-02-12 17:37:17 +0000

    im done

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -10,31 +10,64 @@ public class IrregularPolygon {
 
     // constructor
     public IrregularPolygon() {}
+    
 
     // public methods
     public void add(Point2D.Double aPoint)
     {
         // TODO: Add a point to the IrregularPolygon.
+        myPolygon.add(aPoint);
     }
 
     public double perimeter() {
         // TODO: Calculate the perimeter.
-        return 3.14;
+        double perimeter = 0;
+
+        for (int i = 0; i < myPolygon.size(); i++){
+            Point2D.Double point1 = myPolygon.get(i);
+            Point2D.Double point2 = myPolygon.get((i + 1) % myPolygon.size());
+            perimeter += point1.distance(point2);
+        }
+        return perimeter;
     }
 
     public double area() {
         // TODO: Calculate the area.
-        return 0.0;
+        double area = 0;
+
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double point1 = myPolygon.get(i);
+            Point2D.Double point2 = myPolygon.get((i + 1) % myPolygon.size());
+            area += point1.getX() * point2.getY() - point2.getX() * point1.getY();
+        }
+
+
+        return Math.abs(area) / 2;
     }
 
     public void draw()
     {
+        
         // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
         try {
+            
             // TODO: Draw the polygon.
+            
+
             // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
             DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
-            pen.move(50, 50);
+            Point2D.Double firstPoint = myPolygon.get(0);
+            pen.move(firstPoint.getX(), firstPoint.getY());
+            pen.down();
+
+            for (int i = 1; i < myPolygon.size(); i++) {
+                Point2D.Double currentPoint = myPolygon.get(i);
+                pen.move(currentPoint.getX(), currentPoint.getY());
+            }
+
+            pen.move(firstPoint.getX(), firstPoint.getY());
+            pen.up();
+            
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
